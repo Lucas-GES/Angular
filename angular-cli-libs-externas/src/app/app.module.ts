24b63e082a446c69bc940805ec613697768ca798
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-02-23 14:37:19 -0300

    diretiva ngIf

--- angular-cli-libs-externas/src/app/app.module.ts
+++ angular-cli-libs-externas/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedComponent } from './shared/shared.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SharedComponent
+    SharedComponent,
+    DiretivaNgifComponent
   ],
   imports: [
     BrowserModule,
