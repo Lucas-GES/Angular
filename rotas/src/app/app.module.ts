102f7a74921c336fa44b885aa216921eb3ba3dec
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-02-25 09:51:23 -0300

    Configurando Rotas simples

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterializeButtonModule, MaterializeCardModule } from 'materialize-angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterializeButtonModule,
+    MaterializeCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
