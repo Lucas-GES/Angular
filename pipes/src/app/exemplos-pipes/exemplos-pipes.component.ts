4a8dabaf3929c5715036baa7121e38792f275b88
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-02-25 08:38:49 -0300

    Criando um Pipe Impuro

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -14,6 +14,14 @@ export class ExemplosPipesComponent implements OnInit {
     preco: 295.20,
     dataLancamento: new Date(2016, 6, 19),
     url: 'https://www.amazon.com.br/Java-como-programar-Paul-Deitel-ebook/dp/B01IPIN4WO/ref=sr_1_2?crid=3JHTOBZEWYLH0&keywords=java&qid=1645723115&s=books&sprefix=Java%2Cstripbooks%2C232&sr=1-2'
+  };
+
+  livros: string[] = ['Angular', 'Java'];
+
+  filtro: string = '';
+
+  addCurso(valor: string){
+    this.livros.push(valor);
   }
 
   constructor() { }
