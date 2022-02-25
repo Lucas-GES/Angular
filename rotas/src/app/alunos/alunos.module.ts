40c3be3e60ee435006bfbbb1b5b30789abf38465
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-02-25 15:02:03 -0300

    Rotas Filhas: desenvolvendo as telas

--- rotas/src/app/alunos/alunos.module.ts
+++ rotas/src/app/alunos/alunos.module.ts
@@ -1,20 +1,26 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AlunosComponent } from "./alunos.component";
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from "./alunos.routing.module";
+import { AlunosService } from "./alunos.service";
 
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         AlunosRoutingModule
     ],
     exports: [],
-    declarations: [AlunosComponent, AlunoFormComponent, AlunoDetalheComponent],
-    providers: []
+    declarations: [
+        AlunosComponent,
+         AlunoFormComponent,
+          AlunoDetalheComponent],
+    providers: [AlunosService]
 })
 
 export class AlunosModule {}
