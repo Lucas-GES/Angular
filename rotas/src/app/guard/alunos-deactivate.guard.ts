7b7977659609a973a516b086d00c4e6dc053eb40
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-02-28 10:45:10 -0300

    CanDeactivate com Interface Genérica

--- rotas/src/app/guard/alunos-deactivate.guard.ts
+++ rotas/src/app/guard/alunos-deactivate.guard.ts
@@ -1,19 +1,22 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
-import { AlunoFormComponent } from "../alunos/aluno-form/aluno-form.component";
+//import { AlunoFormComponent } from "../alunos/aluno-form/aluno-form.component";
+import { FormCanDeactivate } from "./form-candeactivate";
 
 @Injectable()
-export class AlunosDeactivateGuard implements CanDeactivate<AlunoFormComponent>{
+export class AlunosDeactivateGuard implements CanDeactivate<FormCanDeactivate>{
     
     canDeactivate(
-        component: AlunoFormComponent,
+        component: FormCanDeactivate,
         currentRoute: ActivatedRouteSnapshot, 
         currentState: RouterStateSnapshot
         ): boolean | Observable<boolean> | Promise<boolean> {
         
             
-            return component.podeMudarRota();
+            //return component.podeMudarRota();
+
+            return component.podeDesativar();
 
     }
 
