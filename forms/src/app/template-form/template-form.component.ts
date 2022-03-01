025295c3134bbaef1c21f5de3741bcf183a4770d
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-03-01 13:45:42 -0300

    Forms populando campos com setValue e patchValue

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -29,7 +29,7 @@ export class TemplateFormComponent implements OnInit {
             'is-invalid': !campo.valid && campo.touched}
   }
 
-  consultaCEP(cep: any){
+  consultaCEP(cep: any, form: any){
     cep = cep.replace(/\D/g, '');
 
     if(cep != null && cep != ""){
@@ -38,11 +38,52 @@ export class TemplateFormComponent implements OnInit {
 
       //Valida o formato do CEP.
       if(validacep.test(cep)) {
+
+        this.resetaDadosForm(form);
         
         this.http.get(`//viacep.com.br/ws/${cep}/json/`)
-        .pipe(map((dados: any) => dados)).subscribe(dados => console.log(dados))
+        .pipe(map((dados: any) => dados)).subscribe(dados => this.populaDadosForm(dados, form))
       }
     }
+  }
+
+  populaDadosForm(dados: any, formulario: any){
+    // formulario.setValue({
+    //   name: formulario.value.name,
+    //   email: formulario.value.email,
+    //   endereço: {
+    //     rua: dados.logradouro,
+    //     cep: dados.cep,
+    //     numero: '',
+    //     complemento: dados.complemento,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf
+    //   }
+    // });
+
+    formulario.form.patchValue({
+      endereço: {
+        rua: dados.logradouro,
+        //cep: dados.cep,        
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm(formulario: { form: { patchValue: (arg0: { endereço: { rua: null; complemento: null; bairro: null; cidade: null; estado: null; }; }) => void; }; }){
+    formulario.form.patchValue({
+      endereço: {
+        rua: null,      
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
 }
