7c57ede8948f59e75981e93a11806fce529478a4
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-03-01 12:11:52 -0300

    Forms pesquisando endereço automaticamente com CEP

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-template-form',
@@ -17,9 +19,30 @@ export class TemplateFormComponent implements OnInit {
     console.log(this.usuario)
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  aplicaCssErro(campo: { valid: any; touched: any; }){
+    return { 'is-valid': campo.valid && campo.touched,
+            'is-invalid': !campo.valid && campo.touched}
+  }
+
+  consultaCEP(cep: any){
+    cep = cep.replace(/\D/g, '');
+
+    if(cep != null && cep != ""){
+      //Expressão regular para validar o CEP.
+      let validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if(validacep.test(cep)) {
+        
+        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
+        .pipe(map((dados: any) => dados)).subscribe(dados => console.log(dados))
+      }
+    }
   }
 
 }
