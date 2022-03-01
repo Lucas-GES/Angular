54914f3de3980ca0a404fdf57943127402b76704
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-03-01 14:49:37 -0300

    Formularios Reativos Sincronizando HTML com formGroup

---
+++ forms/src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormDebugComponent } from './form-debug/form-debug.component';
+
+
+
+@NgModule({
+  declarations: [FormDebugComponent],
+  exports: [FormDebugComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
