cae6c9164305d96deec0f744fbb3ef2c0ef90b88
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-03-01 14:21:34 -0300

    Formularios Reativos configuração

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -4,20 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DataFormComponent } from './data-form/data-form.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TemplateFormModule } from './template-form/template-form.module';
+import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataFormComponent    
+    AppComponent    
   ],
   imports: [
     FormsModule,
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     TemplateFormModule,
+    DataFormModule,
     NgbModule
   ],
   providers: [],
