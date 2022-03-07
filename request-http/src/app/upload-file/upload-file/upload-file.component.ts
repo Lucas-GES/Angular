23e736c4c2a7ec87bdb2797d64274c4225c48045
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-03-07 10:38:11 -0300

    Http Upload de Arquivo: Request com FormData

---
+++ request-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
+
+@Component({
+  selector: 'app-upload-file',
+  templateUrl: './upload-file.component.html',
+  styleUrls: ['./upload-file.component.css']
+})
+export class UploadFileComponent implements OnInit {
+
+  namesFiles!: string;
+
+  files!: Set<File>;
+
+  constructor(private service: UploadFileService) { }
+
+  ngOnInit(): void {
+  }
+
+  onChange(event: any){
+    console.log(event);
+
+    const fileNames = []
+    this.files = new Set();
+
+    const selectedFiles = <FileList>event.srcElement.files;
+    for(let i = 0; i < selectedFiles.length; i++){
+      fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i])
+    }
+    
+    this.namesFiles = [...event.srcElement.files].map((file) => `${file.name}`).join();
+  }
+
+  onUpload(){
+    if(this.files && this.files.size > 0){
+      this.service.upload(this.files, 'http://localhost:8000/upload')
+        .subscribe(response => console.log('Upload Concluido'));
+    }
+  }
+
+}
