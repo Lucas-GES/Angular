d21ea31123c2c7df83029b6ae3cac1c962ab5bbe
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-03-07 11:52:00 -0300

    Http: Criando operador Rxjs customizado

--- request-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ request-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,5 +1,6 @@
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 import { environment } from 'src/environments/environment';
 import { UploadFileService } from '../upload-file.service';
 
@@ -41,20 +42,27 @@ export class UploadFileComponent implements OnInit {
   onUpload(){
     if(this.files && this.files.size > 0){
       this.service.upload(this.files, `${environment.BASE_URL}/upload`)
-        .subscribe((event: HttpEvent<Object>) => {
-          //HttpEventType
-          console.log(event);
-          if(event.type === HttpEventType.Response){
-            console.log('Upload Concluido'); 
-          }else if(event.type === HttpEventType.UploadProgress){
-            if(event.total){
-              const percentDone = Math.round((event.loaded * 100) / event.total);
-              console.log('Progresso', percentDone);
-              this.progress = percentDone;
-            }
-          }
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+        .subscribe(response => console.log('Upload Concluido'));
+        // .subscribe((event: HttpEvent<Object>) => {          
+        //   //console.log(event);
+        //   if(event.type === HttpEventType.Response){
+        //     console.log('Upload Concluido'); 
+        //   }else if(event.type === HttpEventType.UploadProgress){
+        //     if(event.total){
+        //       const percentDone = Math.round((event.loaded * 100) / event.total);
+        //       //console.log('Progresso', percentDone);
+        //       this.progress = percentDone;
+        //     }
+        //   }
                  
-        });
+        // });
     }
   }
 
